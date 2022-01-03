e8649ea09517dfe7df1c56283dfb1a80ea5957f5
Mosh Buyum <75974364+moshB@users.noreply.github.com>
2022-01-03 21:22:43 +0200

    commit

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,7 @@
 
 <x-layout>
+    <botton onclick="test()">hi</botton>
+    <botton onclick="test2()">hi</botton>
     @foreach ($posts as $post)
 
         <article>
@@ -14,3 +16,31 @@
         </article>
     @endforeach
 </x-layout>
+<script>
+    function test(){
+        alert('hello')
+    }
+    function test2() {
+        console.log("go")
+        $.ajax({
+            url: 'http://localhost:8000/sendEmailToUserAfter24Hours',
+            type: 'POST',
+            data: {
+                testId: 259
+            },
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            success: function (data) {
+                console.log('email sent');
+                // activateSnackBarApproval('Email Sent !')
+                // //setTimeout(() => closeSnackBar() , 3000 )
+                // $("#exampleModalCenter").modal('hide');
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                console.log({XMLHttpRequest, textStatus, errorThrown})
+                alert("some error");
+            }
+        })
+    }
+</script>
